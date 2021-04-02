af9221bd4c07fd714d0241d430ea153f97b2c0e8
venkat <venkatsai.nayini@gmail.com>
2021-04-02 16:50:06 +0200

    image url changed

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const API = process.env.REACT_APP_API_URL;
-export const IMAGE_URL='https://papyrus-backend.herokuapp.com/images/homeimage.jpg'
+export const IMAGE_URL='http://papyrus-backend.herokuapp.com/images/homeimage.jpg'
