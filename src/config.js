6320d0c253c5d4954ecafdec02f30c72b2526dba
ramesh bommakanti <ramesh.bommakanti@autorabit.com>
2021-05-15 00:59:42 +0530

    image path changed

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const API = process.env.REACT_APP_API_URL;
-export const IMAGE_URL='https://papyrus-frontend.herokuapp.com/images/homeimage.jpg'
+export const IMAGE_URL='https://papyrus-back.herokuapp.com/images/homeimage.jpg'
